7e89cbfefafeb52cb37a6179d54caf20524f89b1
Lautaro Rodriguez <lautarorodriguez@MacBook-Air-de-Lautaro.local>
2023-10-18 15:07:17 -0300

    fixing bug in contactUs

--- src/component/contactUs/ContactUs.jsx
+++ src/component/contactUs/ContactUs.jsx
@@ -25,19 +25,19 @@ const ContactUs = () => {
                   ? 'animate__slower px-4 animate__animated animate__fadeInUp animate__slow title-contactUs'
                   : 'hidden'
               }
-              style={{color: '#FFC000', fontSize: '25px'}}
+              style={{color: '#FFC000', fontSize: '25px', marginTop: '2rem'}}
             >
               ¿No sabés por dónde empezar?
             </h1>
             <span
-              style={{fontSize: '14px'}}
+              style={{fontSize: '14px', fontWeight: '800'}}
               className={
                 animationSkill
                   ? 'animate__slower container-scenarios animate__animated animate__fadeInUp animate__slow span-contactUs'
                   : 'hidden'
               }
             >
-              Comenza probando nuestro producto.
+              <b>Comenza probando nuestro producto.</b>
             </span>
           </div>
           <div className="container-div-formContactUs">
