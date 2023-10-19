72daedf6235d3585ba279e2bbcddb5068e667ff6
Lautaro Rodriguez <lautarorodriguez@MacBook-Air-de-Lautaro.local>
2023-10-19 15:42:51 -0300

    changed css in p-demo in demoRequest (mobile)

--- src/component/demoRequest/DemoRequest.jsx
+++ src/component/demoRequest/DemoRequest.jsx
@@ -5,9 +5,7 @@ import image from '../../assets/images/image1.png'
 const DemoRequest = () => {
   return (
     <div className="image-with-text-container image1 animate__animated animate__bounceInDown">
-      {/* <Image src={image} alt="Imagen" className="image" /> */}
-
-      <div className="text-overlay flex flex-col">
+      <div className="text-overlay flex flex-col container-div-demoRequest">
         <div className="p-demo-container">
           <p className="p-demo">
             Tus informes de <b style={{color: '#FFC000'}}>Power BI</b>
